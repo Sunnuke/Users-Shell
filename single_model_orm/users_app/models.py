79f262a75709396a92baaa9c2b3197bbb0ab6d3d
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-02 21:36:06 -0800

    Setup Users ERD

--- single_model_orm/users_app/models.py
+++ single_model_orm/users_app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-# class Users(models.Model):
-#     first_name = VARCHAR(255)
-#     last_name = VARCHAR(255)
-#     email_address = VARCHAR(255)
-#     age = INT()
-#     created_at = DATETIME()
-#     updated_at = DATETIME()
+class Users(models.Model):
+    first_name = models.CarField(max_length=45)
+    last_name = models.CarField(max_length=45)
+    email_address = models.CarField(max_length=45)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
