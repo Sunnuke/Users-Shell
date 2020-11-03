28d50bb9d2a9faa11f82894fdd6533839eae4383
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-02 21:23:17 -0800

    link project and app urls

--- single_model_orm/users_app/models.py
+++ single_model_orm/users_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# class Users(models.Model):
+#     first_name = VARCHAR(255)
+#     last_name = VARCHAR(255)
+#     email_address = VARCHAR(255)
+#     age = INT()
+#     created_at = DATETIME()
+#     updated_at = DATETIME()
