e94b7d05758541283cc60a394144fc6b10ddee0a
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-03 17:49:18 -0800

    Added template and static also displayed database user info on template(html)

--- single_model_orm/users_app/views.py
+++ single_model_orm/users_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from .models import Users
 
 # Create your views here.
-# def index(request):
-#     return HttpResponse('Hi Human!')
+def index(request):
+    context = {
+        'all_users': Users.objects.all()
+    # Users.first_name
+    # Users.last_name}
+    # {{Users.email_address}}
+    # {{Users.age}\
+    }
+    return render(request, 'index.html', context)
+
+
+def process():
+    pass
